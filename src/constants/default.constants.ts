88ce72d494e0ae301897050157d6e6c960571e24
rafsan0230 <rafsan0230@gmail.com>
2024-01-15 20:26:44 +0600

    superadmin roles added

--- src/constants/default.constants.ts
+++ src/constants/default.constants.ts
@@ -1,6 +1,6 @@
 export const BCRYPT_SALT_ROUNDS = 10;
 
-export const PossibleRoles = ['admin', 'employee'];
+export const PossibleRoles = ['superadmin', 'admin', 'employee'];
 
 export const JWTtokenNameInCookie = 'access_token';
 export const CurrentUserKey = 'currentUser';
