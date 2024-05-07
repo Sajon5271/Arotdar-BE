52e53df770c4f1923f0d9ae20f11e3ce8029709e
Sajon5271 <sajon5271@gmail.com>
2024-05-07 11:16:50 +0600

    Removed date validation with date string

--- src/profit/dtos/date-range.dto.ts
+++ src/profit/dtos/date-range.dto.ts
@@ -1,24 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class DateRangeDto {
-  @IsDate({ message: 'Is not valid Date' })
+  @IsDateString({}, { message: 'Is not valid Date' })
   @IsNotEmpty()
   @ApiProperty()
   from: Date;
 
-  @IsDate({ message: 'Is not valid Date' })
+  @IsDateString({}, { message: 'Is not valid Date' })
   @IsNotEmpty()
   @ApiProperty()
   to: Date;
 }
+
 export class DateRangeDtoOptional {
-  @IsDate({ message: 'Is not valid Date' })
+  @IsDateString({}, { message: 'Is not valid Date' })
   @IsOptional()
   @ApiProperty()
   from?: Date;
 
-  @IsDate({ message: 'Is not valid Date' })
+  @IsDateString({}, { message: 'Is not valid Date' })
   @IsOptional()
   @ApiProperty()
   to?: Date;
--- src/transaction-logs/dtos/paginated-range.dto.ts
+++ src/transaction-logs/dtos/paginated-range.dto.ts
@@ -4,8 +4,8 @@ import { DateRangeDtoOptional } from '../../profit/dtos/date-range.dto';
 import { PaginationDto } from '../../shared/dtos/paginated.dto';
 
 export class PartnerId {
-  @IsOptional()
   @IsMongoId()
+  @IsOptional()
   @ApiProperty()
   partnerId?: string;
 }
