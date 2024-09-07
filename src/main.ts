94feb051fe0212abed6774c03cb69c393dccf96e
Sirajis Salekin Sajon <sajon5271@gmail.com>
2024-09-07 23:01:24 +0600

    Added frontend to the cors policy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200', 'https://arotdar-fe.vercel.app'],
     credentials: true,
   });
 
